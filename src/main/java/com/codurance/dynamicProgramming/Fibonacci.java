9006c93b8665b084b2433c8559d3a6b70a3c96fc
Tom <thomas.spencer@codurance.com>
2021-01-18 07:42:10 +0000

    Add bottomUpCache

--- src/main/java/com/codurance/dynamicProgramming/Fibonacci.java
+++ src/main/java/com/codurance/dynamicProgramming/Fibonacci.java
@@ -6,4 +6,35 @@ public class Fibonacci {
     if (n == 1) return 1;
     return recursiveFib(n - 1) + recursiveFib(n - 2);
   }
+
+  public static int topDownCache(int n){
+    if (n < 2) return n;
+    int[] cache = new int[n+1];
+    for (int i = 0; i < cache.length; i++){
+      cache[i] = -1;
+    }
+
+    cache[0] = 0;
+    cache[1] = 1;
+    return topDownCache(n, cache);
+  }
+
+  public static int topDownCache(int n, int[] cache){
+    if (cache[n] >= 0) return cache[n];
+    cache[n] = topDownCache(n - 1, cache) + topDownCache(n - 2, cache);
+    return cache[n];
+  }
+
+  public static int bottomUpCache(int n){
+    if (n == 0) return 0;
+
+    int[] cache = new int[n + 1];
+    cache[1] = 1;
+
+    for (int i = 2; i <= n; i++){
+      cache[i] = cache[i - 1] + cache[i - 2];
+    }
+
+    return cache[n];
+  }
 }
--- src/test/java/com/codurance/dynamicProgramming/FibonacciTest.java
+++ src/test/java/com/codurance/dynamicProgramming/FibonacciTest.java
@@ -2,13 +2,28 @@ package com.codurance.dynamicProgramming;
 
 import org.junit.jupiter.api.Test;
 
-import static com.codurance.dynamicProgramming.Fibonacci.recursiveFib;
+import static com.codurance.dynamicProgramming.Fibonacci.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FibonacciTest {
+  // 5s 321ms
   @Test
   void return_fib_n_from_nth_number() {
-    int result = recursiveFib(11);
-    assertEquals(89, result);
+    int result = recursiveFib(45);
+    assertEquals(1134903170, result);
+  }
+
+  // 16 ms
+  @Test
+  void topDownCache_returns_fib_n_from_nth_number() {
+    int result = topDownCache(45);
+    assertEquals(1134903170, result);
+  }
+
+  // 14 ms
+  @Test
+  void bottomUpCache_returns_fib_n_from_nth_number() {
+    int result = bottomUpCache(45);
+    assertEquals(1134903170, result);
   }
 }
