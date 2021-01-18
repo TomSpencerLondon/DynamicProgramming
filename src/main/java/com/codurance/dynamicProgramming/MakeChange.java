cd507e940cb645ef5e07ccae57e28464aafc6983
Tom <thomas.spencer@codurance.com>
2021-01-18 16:20:11 +0000

    Added makeChange for 3 possible solutions

--- src/main/java/com/codurance/dynamicProgramming/MakeChange.java
+++ src/main/java/com/codurance/dynamicProgramming/MakeChange.java
@@ -18,4 +18,46 @@ public class MakeChange {
 
     return minCoins + 1;
   }
+
+  public static int makeChangeDynamic(int c){
+    int[] cache = new int[c + 1];
+    for (int i = 1; i < c + 1; i++){
+      cache[i] = -1;
+    }
+    return makeChangeDynamic(c, cache);
+  }
+
+  private static int makeChangeDynamic(int c, int[] cache){
+    if (cache[c] >= 0) return cache[c];
+
+    int minCoins = Integer.MAX_VALUE;
+    for (int coin: coins) {
+      if (c - coin >= 0){
+        int currMinCoins = makeChangeDynamic(c - coin, cache);
+        if (currMinCoins < minCoins){
+          minCoins = currMinCoins;
+        }
+      }
+    }
+
+    cache[c] = minCoins + 1;
+    return cache[c];
+  }
+
+  public static int makeChangeTopDown(int c) {
+    int[] cache = new int[c + 1];
+    for (int i  = 1; i <= c; i++){
+      int minCoins = Integer.MAX_VALUE;
+      for (int coin: coins){
+        if (i - coin >= 0){
+          int currCoins = cache[i - coin] + 1;
+          if (currCoins < minCoins){
+            minCoins = currCoins;
+          }
+        }
+      }
+      cache[i] = minCoins;
+    }
+    return cache[c];
+  }
 }
--- src/test/java/com/codurance/dynamicProgramming/MakeChangeTest.java
+++ src/test/java/com/codurance/dynamicProgramming/MakeChangeTest.java
@@ -1,9 +1,10 @@
 package com.codurance.dynamicProgramming;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import static com.codurance.dynamicProgramming.MakeChange.makeChange;
+import static com.codurance.dynamicProgramming.MakeChange.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MakeChangeTest {
@@ -17,6 +18,34 @@ public class MakeChangeTest {
   )
   void returns_correct_total_coins_for_amount_of_change(int input, int expected) {
     int output = makeChange(input);
+
+    assertEquals(expected, output);
+  }
+
+  @ParameterizedTest
+  @CsvSource(
+      {
+          "1, 1",
+          "6, 2",
+          "49, 7"
+      }
+  )
+  void dynamic_make_change_returns_total_coins_for_change(int input, int expected) {
+    int output = makeChangeDynamic(input);
+
+    assertEquals(expected, output);
+  }
+
+  @ParameterizedTest
+  @CsvSource(
+      {
+          "1, 1",
+          "6, 2",
+          "49, 7"
+      }
+  )
+  void top_down_make_change_returns_total_coins_for_change(int input, int expected) {
+    int output = makeChangeTopDown(input);
 
     assertEquals(expected, output);
   }
